05d35a46160dba319822012233e4d05840ee6976
leonardo honda <leozinhohondams@gmail.com>
2026-03-10 16:05:33 -0400

    feat: add error handling to prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,10 +7,46 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (error) {
+      throw new InternalServerError({
+        cause: 'Falha ao conectar com banco de dados',
+        statusCode: 500,
+      });
+    }
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    try {
+      await this.$disconnect();
+    } catch (error) {
+      throw new InternalServerError({
+        cause: 'Falha ao desconectar do banco de dados',
+        statusCode: 500,
+      });
+    }
+  }
+}
+
+class InternalServerError extends Error {
+  action?: string;
+  statusCode?: number;
+  constructor({ cause, statusCode }: { cause?: string; statusCode?: number }) {
+    super('Um erro interno não esperado aconteceu.', {
+      cause,
+    });
+    this.name = 'InternalServerError';
+    this.action = 'Entre em contato com o suporte.';
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
   }
 }
