7f6bdba2232f5e1f043959515da81ee17676ac0c
leonardo honda <leozinhohondams@gmail.com>
2026-03-15 22:29:30 -0400

    chore: fix folder typo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Patch, Body, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { CurrentUser } from '../common/docorators/current-user.decorator';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
 
 @Controller('me')
 @UseGuards(JwtAuthGuard)
